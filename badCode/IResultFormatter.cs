1a2ebfb581cde8ea13db8870e95e7b745c392a31
maurmni <ylavartalo@outlook.com>
2025-11-15 23:06:39 +0500

    исправление багов и легаси проблем

---
+++ badCode/IResultFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace badCode
+{
+    public interface IResultFormatter
+    {
+        void DisplayAnalysisResult(TextAnalysisResult result);
+        void DisplayCharacterFrequency(Dictionary<char, int> frequency);
+        void DisplayComparisonResult(TextComparisonResult result);
+    }
+}
